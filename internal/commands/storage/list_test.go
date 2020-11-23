32c50ff5888bbd3c810ba9bdab7cd99f19b9cab0
Zsolt Balvanyos <zsolt.balvanyos@gmail.com>
2020-11-23 14:47:27 +0000

    Additional filters by type
    List storages by type and access i.e:
    ./cli storage list --backup --public
    will print all public backup storages

--- internal/commands/storage/list_test.go
+++ internal/commands/storage/list_test.go
@@ -2,91 +2,102 @@ package storage
 
 import (
 	"bytes"
-	"github.com/stretchr/testify/mock"
 	"testing"
 
 	"github.com/UpCloudLtd/cli/internal/commands"
 	"github.com/UpCloudLtd/cli/internal/config"
+	"github.com/UpCloudLtd/cli/internal/mocks"
 	"github.com/UpCloudLtd/upcloud-go-api/upcloud"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
-var storage1 = upcloud.Storage{
-	UUID:   Uuid1,
-	Title:  Title1,
-	Access: "private",
-	State:  "maintenance",
-	Type:   "backup",
-	Zone:   "fi-hel1",
-	Size:   40,
-	Tier:   "maxiops",
-}
-
-var storage2 = upcloud.Storage{
-	UUID:   Uuid2,
-	Title:  Title2,
-	Access: "private",
-	State:  "online",
-	Type:   "normal",
-	Zone:   "fi-hel1",
-	Size:   40,
-	Tier:   "maxiops",
-}
-
-var storage3 = upcloud.Storage{
-	UUID:   Uuid3,
-	Title:  Title3,
-	Access: "public",
-	State:  "online",
-	Type:   "normal",
-	Zone:   "fi-hel1",
-	Size:   10,
-	Tier:   "maxiops",
-}
-
 func TestListStorages(t *testing.T) {
+
+	var Storage4 = Storage1
+	Storage4.Title = "mock-storage-title4"
+	Storage4.Type = upcloud.StorageTypeCDROM
+	var Storage5 = Storage1
+	Storage5.Title = "mock-storage-title5"
+	Storage5.Type = upcloud.StorageTypeTemplate
+	var Storage6 = Storage3
+	Storage6.Title = "mock-storage-title6"
+	Storage6.Type = upcloud.StorageTypeBackup
+
+	storages := upcloud.Storages{Storages: []upcloud.Storage{Storage1, Storage2, Storage3, Storage4, Storage5, Storage6}}
 
 	for _, testcase := range []struct {
 		name    string
 		private bool
 		public  bool
+		args    []string
 		testFn  func(res upcloud.Storages, e error)
 	}{
 		{
-			name:    "List storages",
-			private: true,
-			public:  true,
+			name: "List storages",
+			args: []string{"--private", "--public"},
 			testFn: func(res upcloud.Storages, e error) {
-				assert.Equal(t, 2, len(res.Storages))
+				assert.ElementsMatch(t, res.Storages, storages.Storages)
 				assert.Nil(t, e)
 			},
 		},
 		{
-			name:    "List private storages",
-			private: true,
-			public:  false,
+			name: "List private storages",
+			args: []string{"--private"},
 			testFn: func(res upcloud.Storages, e error) {
-				assert.Equal(t, 2, len(res.Storages))
+				assert.ElementsMatch(t, res.Storages, []upcloud.Storage{Storage1, Storage2, Storage4, Storage5})
 				assert.Nil(t, e)
 			},
 		},
 		{
-			name:    "List public storages",
-			private: false,
-			public:  true,
+			name: "List public storages",
+			args: []string{"--public"},
 			testFn: func(res upcloud.Storages, e error) {
-				assert.Equal(t, 2, len(res.Storages))
+				assert.ElementsMatch(t, res.Storages, []upcloud.Storage{Storage3, Storage6})
+				assert.Nil(t, e)
+			},
+		},
+		{
+			name: "List private by default",
+			args: []string{},
+			testFn: func(res upcloud.Storages, e error) {
+				assert.ElementsMatch(t, res.Storages, []upcloud.Storage{Storage1, Storage2, Storage4, Storage5})
+				assert.Nil(t, e)
+			},
+		},
+		{
+			name: "List cdrom",
+			args: []string{"--cdrom"},
+			testFn: func(res upcloud.Storages, e error) {
+				assert.ElementsMatch(t, res.Storages, []upcloud.Storage{Storage4})
+				assert.Nil(t, e)
+			},
+		},
+		{
+			name: "List public backup",
+			args: []string{"--public", "--backup"},
+			testFn: func(res upcloud.Storages, e error) {
+				assert.ElementsMatch(t, res.Storages, []upcloud.Storage{Storage6})
+				assert.Nil(t, e)
+			},
+		},
+		{
+			name: "List public template",
+			args: []string{"--public", "--template"},
+			testFn: func(res upcloud.Storages, e error) {
+				assert.ElementsMatch(t, res.Storages, []upcloud.Storage{})
 				assert.Nil(t, e)
 			},
 		},
 	} {
 		t.Run(testcase.name, func(t *testing.T) {
+			stgs := storages
+			mss := new(mocks.MockStorageService)
+			mss.On("GetStorages", mock.Anything).Return(&stgs, nil)
 
-			mss := MockStorageService()
-			mss.On("GetStorages", mock.Anything).Return(storages, nil)
 			lc := commands.BuildCommand(ListCommand(mss), nil, config.New(viper.New()))
-
+			_ = mocks.SetFlags(lc, testcase.args)
 			res, err := lc.MakeExecuteCommand()([]string{})
 			result := res.(*upcloud.Storages)
 			testcase.testFn(*result, err)
@@ -95,16 +106,8 @@ func TestListStorages(t *testing.T) {
 }
 
 func TestListStoragesOutput(t *testing.T) {
-	storages := &upcloud.Storages{
-		Storages: []upcloud.Storage{
-			storage1,
-			storage2,
-			storage3,
-		},
-	}
 
-	mss := MockStorageService()
-	lc := commands.BuildCommand(ListCommand(mss), nil, config.New(viper.New()))
+	lc := commands.BuildCommand(ListCommand(new(mocks.MockStorageService)), nil, config.New(viper.New()))
 
 	expected := `
  UUID                                   Title                 Zone      State         Type     Size   Tier      Created 
