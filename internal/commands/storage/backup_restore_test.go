8d05918fe159357f6c717e1d702a09f11ebef988
Zsolt Balvanyos <zsolt.balvanyos@gmail.com>
2020-11-23 14:36:28 +0000

    Implement backup related commands
    Create and restore are subcommands of backup command.

---
+++ internal/commands/storage/backup_restore_test.go
@@ -0,0 +1,44 @@
+package storage
+
+import (
+	"github.com/UpCloudLtd/cli/internal/commands"
+	"github.com/UpCloudLtd/cli/internal/config"
+	"github.com/UpCloudLtd/cli/internal/mocks"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"testing"
+)
+
+func TestRestoreBackupCommand(t *testing.T) {
+	methodName := "RestoreBackup"
+
+	for _, test := range []struct {
+		name        string
+		args        []string
+		methodCalls int
+	}{
+		{
+			name:        "Backend called",
+			args:        []string{},
+			methodCalls: 1,
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			mss := MockStorageService()
+			mss.On(methodName, mock.Anything).Return(nil, nil)
+
+			tc := commands.BuildCommand(RestoreBackupCommand(mss), nil, config.New(viper.New()))
+			mocks.SetFlags(tc, test.args)
+
+			results, err := tc.MakeExecuteCommand()([]string{Storage2.UUID})
+			for _, result := range results.([]interface{}) {
+				assert.Equal(t, nil, result)
+			}
+
+			assert.Nil(t, err)
+
+			mss.AssertNumberOfCalls(t, methodName, test.methodCalls)
+		})
+	}
+}
