269de8a4bfd368b969f0a36610b57c4269a4af1a
Codeslayer666 <samir.alkurdi@upcloud.com>
2021-01-13 18:19:57 +0200

    Move attach-storage and detach-storage under server to a similar submenu as network-interface.

---
+++ internal/commands/server_storage/attach_test.go
@@ -0,0 +1,125 @@
+package server_storage
+
+import (
+	"github.com/UpCloudLtd/cli/internal/commands"
+	"github.com/UpCloudLtd/cli/internal/commands/storage"
+	"github.com/UpCloudLtd/cli/internal/config"
+	"github.com/UpCloudLtd/upcloud-go-api/upcloud"
+	"github.com/UpCloudLtd/upcloud-go-api/upcloud/request"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"testing"
+)
+
+func TestAttachStorageCommand(t *testing.T) {
+	methodName := "AttachStorage"
+
+	var Storage1 = upcloud.Storage{
+		UUID:   Uuid1,
+		Title:  Title1,
+		Access: "private",
+		State:  "maintenance",
+		Type:   "backup",
+		Zone:   "fi-hel1",
+		Size:   40,
+		Tier:   "maxiops",
+	}
+	var storages = &upcloud.Storages{
+		Storages: []upcloud.Storage{
+			Storage1,
+		},
+	}
+
+	var Server1 = upcloud.Server{
+		CoreNumber:   1,
+		Hostname:     "server-1-hostname",
+		License:      0,
+		MemoryAmount: 1024,
+		Plan:         "server-1-plan",
+		Progress:     0,
+		State:        "started",
+		Tags:         nil,
+		Title:        "server-1-title",
+		UUID:         "1fdfda29-ead1-4855-b71f-1e33eb2ca9de",
+		Zone:         "fi-hel1",
+	}
+
+	var servers = &upcloud.Servers{
+		Servers: []upcloud.Server{
+			Server1,
+		},
+	}
+
+	var serverDetails = upcloud.ServerDetails{
+		Server: upcloud.Server{
+			UUID:  Uuid1,
+			State: upcloud.ServerStateStarted,
+		},
+		VideoModel: "vga",
+		Firewall:   "off",
+	}
+
+	for _, test := range []struct {
+		name       string
+		args       []string
+		attacheReq request.AttachStorageRequest
+		error      string
+	}{
+		{
+			name:  "storage is missing",
+			args:  []string{},
+			error: "storage is required",
+		},
+		{
+			name: "use default values",
+			args: []string{"--storage", Storage1.Title},
+			attacheReq: request.AttachStorageRequest{
+				ServerUUID:  Server1.UUID,
+				Type:        upcloud.StorageTypeDisk,
+				Address:     "virtio",
+				StorageUUID: Storage1.UUID,
+				BootDisk:    0,
+			},
+		},
+		{
+			name: "flags mapped to the correct field",
+			args: []string{
+				"--storage", Storage1.Title,
+				"--type", "cdrom",
+				"--address", "ide",
+				"--boot-disk", "1",
+			},
+			attacheReq: request.AttachStorageRequest{
+				ServerUUID:  Server1.UUID,
+				Type:        upcloud.StorageTypeCDROM,
+				Address:     "ide",
+				StorageUUID: Storage1.UUID,
+				BootDisk:    1,
+			},
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			CachedServers = nil
+			storage.CachedStorages = nil
+
+			mServerService := MockServerService{}
+			mServerService.On("GetServers", mock.Anything).Return(servers, nil)
+
+			mStorageService := MockStorageService{}
+			mStorageService.On(methodName, &test.attacheReq).Return(&serverDetails, nil)
+			mStorageService.On("GetStorages", mock.Anything).Return(storages, nil)
+
+			cc := commands.BuildCommand(AttachCommand(&mServerService, &mStorageService), nil, config.New(viper.New()))
+			cc.SetFlags(test.args)
+
+			_, err := cc.MakeExecuteCommand()([]string{Server1.UUID})
+
+			if test.error != "" {
+				assert.Equal(t, test.error, err.Error())
+			} else {
+				mStorageService.AssertNumberOfCalls(t, methodName, 1)
+			}
+		})
+	}
+}
---
+++ internal/commands/server_storage/detach_test.go
@@ -0,0 +1,83 @@
+package server_storage
+
+import (
+	"github.com/UpCloudLtd/cli/internal/commands"
+	"github.com/UpCloudLtd/cli/internal/commands/storage"
+	"github.com/UpCloudLtd/cli/internal/config"
+	"github.com/UpCloudLtd/upcloud-go-api/upcloud"
+	"github.com/UpCloudLtd/upcloud-go-api/upcloud/request"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"testing"
+)
+
+func TestDetachCommand(t *testing.T) {
+	methodName := "DetachStorage"
+
+	var Server1 = upcloud.Server{
+		CoreNumber:   1,
+		Hostname:     "server-1-hostname",
+		License:      0,
+		MemoryAmount: 1024,
+		Plan:         "server-1-plan",
+		Progress:     0,
+		State:        "started",
+		Tags:         nil,
+		Title:        "server-1-title",
+		UUID:         "1fdfda29-ead1-4855-b71f-1e33eb2ca9de",
+		Zone:         "fi-hel1",
+	}
+	var servers = &upcloud.Servers{
+		Servers: []upcloud.Server{
+			Server1,
+		},
+	}
+
+	details := upcloud.ServerDetails{
+		Server: Server1,
+	}
+
+	for _, test := range []struct {
+		name      string
+		args      []string
+		detachReq request.DetachStorageRequest
+		error     string
+	}{
+		{
+			name:  "Address missing",
+			args:  []string{},
+			error: "address is required",
+		},
+		{
+			name: "Address provided",
+			args: []string{"--address", "ide"},
+			detachReq: request.DetachStorageRequest{
+				Address:    "ide",
+				ServerUUID: Server1.UUID,
+			},
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			CachedServers = nil
+			storage.CachedStorages = nil
+
+			mServerService := MockServerService{}
+			mServerService.On("GetServers", mock.Anything).Return(servers, nil)
+
+			mStorageService := MockStorageService{}
+			mStorageService.On(methodName, &test.detachReq).Return(&details, nil)
+
+			tc := commands.BuildCommand(DetachCommand(&mServerService, &mStorageService), nil, config.New(viper.New()))
+			tc.SetFlags(test.args)
+
+			_, err := tc.MakeExecuteCommand()([]string{Server1.UUID})
+
+			if test.error != "" {
+				assert.Equal(t, test.error, err.Error())
+			} else {
+				mStorageService.AssertNumberOfCalls(t, methodName, 1)
+			}
+		})
+	}
+}
