30c54262bb44ee06735641eae04a6954cc288c3f
Codeslayer666 <samir.alkurdi@upcloud.com>
2021-03-04 12:59:11 +0200

    Info for enable / disable the firewall

--- internal/commands/serverfirewall/server_firewall.go
+++ internal/commands/serverfirewall/server_firewall.go
@@ -10,7 +10,7 @@ const (
 )
 
 func BaseServerFirewallCommand() commands.Command {
-	return &serverFirewallCommand{commands.New("firewall", "Manage server firewall rules.")}
+	return &serverFirewallCommand{commands.New("firewall", "Manage server firewall rules. Enabling or disabling the firewall is done in server modify.")}
 }
 
 type serverFirewallCommand struct {
