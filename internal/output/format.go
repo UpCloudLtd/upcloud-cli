04c9ed97ef7115902e538ec1bbbf69cb2cccf32c
Matias Lahti <matias.lahti@upcloud.com>
2021-04-06 17:57:43 +0300

    fix(output): format upcloud.Boolean correctly too

--- internal/output/format.go
+++ internal/output/format.go
@@ -3,13 +3,19 @@ package output
 import (
 	"fmt"
 	"github.com/UpCloudLtd/cli/internal/ui"
+	"github.com/UpCloudLtd/upcloud-go-api/upcloud"
 	"github.com/jedib0t/go-pretty/v6/text"
 )
 
 // BoolFormat returns val formatted as a boolean
 func BoolFormat(val interface{}) (text.Colors, string, error) {
-	if boolVal, ok := val.(bool); ok {
-		if boolVal {
+	if vb, ok := val.(bool); ok {
+		if vb {
+			return ui.DefaultBooleanColoursTrue, "yes", nil
+		}
+		return ui.DefaultBooleanColoursFalse, "no", nil
+	} else if upb, ok := val.(upcloud.Boolean); ok {
+		if upb == upcloud.True {
 			return ui.DefaultBooleanColoursTrue, "yes", nil
 		}
 		return ui.DefaultBooleanColoursFalse, "no", nil
