26831f323c5e744d26711fb5f625a9b28d7ac09d
Amine Kherbouche <kaminek92@gmail.com>
2021-04-06 02:09:01 +0200

    chore(account): fmt output struct
    Signed-off-by: Amine Kherbouche <kaminek92@gmail.com>

--- internal/commands/account/show.go
+++ internal/commands/account/show.go
@@ -27,20 +27,56 @@ func (s *showCommand) Execute(exec commands.Executor, _ string) (output.Command,
 	}
 	return output.Details{
 		Sections: []output.DetailSection{
-			{Rows: []output.DetailRow{
-				{Title: "Username:", Key: "username", Value: account.UserName},
-				{Title: "Credits:", Key: "credits", Value: formatCredits(account.Credits)},
-			}},
-			{Title: "Resource Limits:", Key: "resource_limits", Rows: []output.DetailRow{
-				{Title: "Cores:", Key: "cores", Value: account.ResourceLimits.Cores},
-				{Title: "Detached Floating IPs:", Key: "detached_floating_ips", Value: account.ResourceLimits.DetachedFloatingIps},
-				{Title: "Memory:", Key: "memory", Value: account.ResourceLimits.Memory},
-				{Title: "Networks:", Key: "networks", Value: account.ResourceLimits.Networks},
-				{Title: "Public IPv4:", Key: "public_ipv4", Value: account.ResourceLimits.PublicIPv4},
-				{Title: "Public IPv6:", Key: "public_ipv6", Value: account.ResourceLimits.PublicIPv6},
-				{Title: "Storage HDD:", Key: "storage_hdd", Value: account.ResourceLimits.StorageHDD},
-				{Title: "Storage SSD:", Key: "storage_ssd", Value: account.ResourceLimits.StorageSSD},
-			}},
+			{
+				Rows: []output.DetailRow{
+					{Title: "Username:", Key: "username", Value: account.UserName},
+					{Title: "Credits:", Key: "credits", Value: formatCredits(account.Credits)},
+				},
+			},
+			{
+				Title: "Resource Limits:", Key: "resource_limits", Rows: []output.DetailRow{
+					{
+						Title: "Cores:",
+						Key:   "cores",
+						Value: account.ResourceLimits.Cores,
+					},
+					{
+						Title: "Detached Floating IPs:",
+						Key:   "detached_floating_ips",
+						Value: account.ResourceLimits.DetachedFloatingIps,
+					},
+					{
+						Title: "Memory:",
+						Key:   "memory",
+						Value: account.ResourceLimits.Memory,
+					},
+					{
+						Title: "Networks:",
+						Key:   "networks",
+						Value: account.ResourceLimits.Networks,
+					},
+					{
+						Title: "Public IPv4:",
+						Key:   "public_ipv4",
+						Value: account.ResourceLimits.PublicIPv4,
+					},
+					{
+						Title: "Public IPv6:",
+						Key:   "public_ipv6",
+						Value: account.ResourceLimits.PublicIPv6,
+					},
+					{
+						Title: "Storage HDD:",
+						Key:   "storage_hdd",
+						Value: account.ResourceLimits.StorageHDD,
+					},
+					{
+						Title: "Storage SSD:",
+						Key:   "storage_ssd",
+						Value: account.ResourceLimits.StorageSSD,
+					},
+				},
+			},
 		},
 	}, nil
 }
