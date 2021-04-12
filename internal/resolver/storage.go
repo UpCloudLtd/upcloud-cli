ec0688cc2006dc52446aaac9326536a0616ced85
Matias Lahti <matias.lahti@upcloud.com>
2021-04-12 12:33:59 +0300

    feat(resolver/storage): implement querying of cache to storageresolver

--- internal/resolver/storage.go
+++ internal/resolver/storage.go
@@ -1,26 +1,25 @@
 package resolver
 
 import (
+	"errors"
 	internal "github.com/UpCloudLtd/cli/internal/service"
+	"github.com/UpCloudLtd/upcloud-go-api/upcloud"
 
 	"github.com/UpCloudLtd/upcloud-go-api/upcloud/request"
 )
 
 // CachingStorage implements resolver for storages, caching the results
-type CachingStorage struct{}
+type CachingStorage struct {
+	cachedStorages *upcloud.Storages
+}
 
 // make sure we implement the ResolutionProvider interface
-var _ ResolutionProvider = CachingStorage{}
+var _ ResolutionProvider = &CachingStorage{}
 
-// Get implements ResolutionProvider.Get
-func (s CachingStorage) Get(svc internal.AllServices) (Resolver, error) {
-	storages, err := svc.GetStorages(&request.GetStoragesRequest{})
-	if err != nil {
-		return nil, err
-	}
+func storageMatcher(cached []upcloud.Storage) func(arg string) (uuid string, err error) {
 	return func(arg string) (uuid string, err error) {
 		rv := ""
-		for _, storage := range storages.Storages {
+		for _, storage := range cached {
 			if storage.Title == arg || storage.UUID == arg {
 				if rv != "" {
 					return "", AmbiguousResolutionError(arg)
@@ -34,10 +33,42 @@ func (s CachingStorage) Get(svc internal.AllServices) (Resolver, error) {
 		}
 
 		return "", NotFoundError(arg)
-	}, nil
+	}
+}
+
+// Get implements ResolutionProvider.Get
+func (s *CachingStorage) Get(svc internal.AllServices) (Resolver, error) {
+	var err error
+	s.cachedStorages, err = svc.GetStorages(&request.GetStoragesRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return storageMatcher(s.cachedStorages.Storages), nil
 }
 
 // PositionalArgumentHelp implements resolver.ResolutionProvider
-func (s CachingStorage) PositionalArgumentHelp() string {
+func (s *CachingStorage) PositionalArgumentHelp() string {
 	return "<UUID/Title...>"
+}
+
+// Resolve is a helper method for commands to resolve networks inside Execute(), outside arguments
+func (s *CachingStorage) Resolve(arg string) (resolved string, err error) {
+	if s.cachedStorages == nil {
+		return "", errors.New("caching storage does not have a cache initialized")
+	}
+	return storageMatcher(s.cachedStorages.Storages)(arg)
+
+}
+
+// GetCached is a helper method for commands to use when they need to get an item from the cached results
+func (s *CachingStorage) GetCached(uuid string) (upcloud.Storage, error) {
+	if s.cachedStorages == nil {
+		return upcloud.Storage{}, errors.New("caching storages does not have a cache initialized")
+	}
+	for _, storage := range s.cachedStorages.Storages {
+		if storage.UUID == uuid {
+			return storage, nil
+		}
+	}
+	return upcloud.Storage{}, NotFoundError(uuid)
 }
