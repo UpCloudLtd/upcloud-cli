99632b27717ca1824a2288debfc439fb030a91e2
Amine Kherbouche <kaminek92@gmail.com>
2021-04-21 10:57:07 +0200

    test: validate input configs (#67)
    * chore(output): get output writer from cobra
    
    Signed-off-by: Amine Kherbouche <kaminek92@gmail.com>
    
    * chore(test): add input testing
    
    Signed-off-by: Amine Kherbouche <kaminek92@gmail.com>

--- cmd/upctl/main.go
+++ cmd/upctl/main.go
@@ -3,28 +3,12 @@ package main
 import (
 	"os"
 
-	"github.com/UpCloudLtd/upcloud-cli/internal/commands/all"
-	"github.com/UpCloudLtd/upcloud-cli/internal/config"
 	"github.com/UpCloudLtd/upcloud-cli/internal/core"
 )
 
-// BootstrapCLI is the CLI entrypoint
-func BootstrapCLI(args []string) error {
-
-	conf := config.New()
-	rootCmd := core.BuildRootCmd(args, conf)
-
-	all.BuildCommands(&rootCmd, conf)
-	if err := rootCmd.Execute(); err != nil {
-		return err
-	}
-
-	return nil
-}
-
 func main() {
 
-	if err := BootstrapCLI(os.Args); err != nil {
+	if err := core.BootstrapCLI(os.Args); err != nil {
 		os.Exit(1)
 	}
 }
