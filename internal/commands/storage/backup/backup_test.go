b027e1a9489e74c1a832f5d87a754851773b9825
Toni Kangas <toni.kangas@upcloud.com>
2022-07-25 16:20:21 +0300

    refactor(storage): move storage backup commands into a sub-directory
    This syncs directory structure with command structure similarly than with server and database commands.

---
+++ internal/commands/storage/backup/backup_test.go
@@ -0,0 +1,8 @@
+package storagebackup
+
+const (
+	Title1 = "mock-storage-title1"
+	Title2 = "mock-storage-title2"
+	UUID1  = "0127dfd6-3884-4079-a948-3a8881df1a7a"
+	UUID2  = "012bde1d-f0e7-4bb2-9f4a-74e1f2b49c07"
+)
