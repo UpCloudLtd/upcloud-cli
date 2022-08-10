99e93f89cafdbb4d6b3d17f39318bb1b9a308a3e
Toni Kangas <toni.kangas@upcloud.com>
2022-08-10 14:27:20 +0300

    refactor(lb): add common format function for possibly unknown strings

--- internal/ui/text.go
+++ internal/ui/text.go
@@ -59,3 +59,16 @@ func FormatTime(tv time.Time) string {
 	}
 	return tv.Format(time.RFC3339)
 }
+
+// FormatPossiblyUnkonwnString outputs "Unknown" in light black if input value is an empty string, otherwise passesthrough the input value.
+func FormatPossiblyUnknownString(val interface{}) (text.Colors, string, error) {
+	str, ok := val.(string)
+	if !ok {
+		return nil, "", fmt.Errorf("cannot parse %T, expected string", val)
+	}
+
+	if str == "" {
+		return nil, text.FgHiBlack.Sprint("unknown"), nil
+	}
+	return nil, str, nil
+}
