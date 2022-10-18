e9c93d26c4c15481481a26d04dd15b449557d420
Ville Skyttä <ville.skytta@upcloud.com>
2022-10-18 16:19:28 +0300

    feat(completion): complete types for `database plans`

--- internal/completion/database.go
+++ internal/completion/database.go
@@ -24,3 +24,22 @@ func (s Database) CompleteArgument(svc service.AllServices, toComplete string) (
 	}
 	return MatchStringPrefix(vals, toComplete, true), cobra.ShellCompDirectiveNoFileComp
 }
+
+// DatabaseType implements argument completion for database types.
+type DatabaseType struct{}
+
+// make sure DatabaseType implements the interface
+var _ Provider = DatabaseType{}
+
+// CompleteArgument implements completion.Provider
+func (s DatabaseType) CompleteArgument(svc service.AllServices, toComplete string) ([]string, cobra.ShellCompDirective) {
+	dbTypes, err := svc.GetManagedDatabaseServiceTypes(&request.GetManagedDatabaseServiceTypesRequest{})
+	if err != nil {
+		return None(toComplete)
+	}
+	var vals []string
+	for _, t := range dbTypes {
+		vals = append(vals, t.Name)
+	}
+	return MatchStringPrefix(vals, toComplete, true), cobra.ShellCompDirectiveNoFileComp
+}
