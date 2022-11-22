228e2f71b3bfff419f5ced6032cc64b7975fd699
Ville Välimäki <ville.valimaki@upcloud.com>
2022-11-22 09:47:58 +0200

    feat(kubernetes): write flag for config

--- internal/commands/kubernetes/config.go
+++ internal/commands/kubernetes/config.go
@@ -7,17 +7,18 @@ import (
 	"github.com/UpCloudLtd/upcloud-cli/v2/internal/completion"
 	"github.com/UpCloudLtd/upcloud-cli/v2/internal/output"
 	"github.com/UpCloudLtd/upcloud-cli/v2/internal/resolver"
-	"github.com/UpCloudLtd/upcloud-go-api/v4/upcloud/request"
 
+	"github.com/UpCloudLtd/upcloud-go-api/v4/upcloud/request"
 	"github.com/spf13/pflag"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/clientcmd/api"
 )
 
 type configCommand struct {
 	*commands.BaseCommand
 	resolver.CachingKubernetes
 	completion.Kubernetes
-	write string
+	pathOptions *clientcmd.PathOptions
 }
 
 // ConfigCommand creates the "connection config" command
@@ -30,33 +31,59 @@ func ConfigCommand() commands.Command {
 			`upctl kubernetes config 0fa980c4-0e4f-460b-9869-11b7bd62b831 --output yaml --write $KUBECONFIG`,
 			`upctl kubernetes config 0fa980c4-0e4f-460b-9869-11b7bd62b831 --output yaml --write ./my_kubeconfig.yaml`,
 		),
+		pathOptions: clientcmd.NewDefaultPathOptions(),
 	}
 }
 
 // InitCommand implements Command.InitCommand
-func (s *configCommand) InitCommand() {
+func (c *configCommand) InitCommand() {
 	flagSet := &pflag.FlagSet{}
-	flagSet.StringVar(&s.write, "write", "", "Target file path where to write config output. Default value \"\" (empty string) implies no writing will be done.")
-	s.AddFlags(flagSet)
+	flagSet.StringVar(
+		&c.pathOptions.LoadingRules.ExplicitPath,
+		"write",
+		"",
+		"Absolute path for writing output. If the file exists, results will be merged. Default value \"\" implies no writing will be done.")
+	c.AddFlags(flagSet)
 }
 
 // Execute implements commands.MultipleArgumentCommand
-func (s *configCommand) Execute(exec commands.Executor, uuid string) (output.Output, error) {
+func (c *configCommand) Execute(exec commands.Executor, uuid string) (output.Output, error) {
 	svc := exec.All()
 
 	msg := fmt.Sprintf("Getting kubeconfig for Kubernetes cluster %s", uuid)
 	exec.PushProgressStarted(msg)
 
-	kubeconfig, err := svc.GetKubernetesKubeconfig(&request.GetKubernetesKubeconfigRequest{
+	resp, err := svc.GetKubernetesKubeconfig(&request.GetKubernetesKubeconfigRequest{
 		UUID: uuid,
 	})
 	if err != nil {
 		return commands.HandleError(exec, msg, err)
 	}
 
-	uksConfig, err := clientcmd.Load([]byte(kubeconfig))
+	uksConfig, err := clientcmd.Load([]byte(resp))
 	if err != nil {
 		return commands.HandleError(exec, msg, err)
+	}
+
+	exec.PushProgressSuccess(msg)
+
+	if c.Cobra().Flag("write").Changed {
+		msg := fmt.Sprintf("Writing kubeconfig for Kubernetes cluster %s to destination %s", uuid, c.pathOptions.GetDefaultFilename())
+		exec.PushProgressStarted(msg)
+
+		startingConfig, err := c.pathOptions.GetStartingConfig()
+		if err != nil {
+			return commands.HandleError(exec, msg, err)
+		}
+
+		err = clientcmd.ModifyConfig(c.pathOptions, mergeConfig(startingConfig, uksConfig), false)
+		if err != nil {
+			return commands.HandleError(exec, msg, err)
+		}
+
+		exec.PushProgressSuccess(msg)
+
+		return output.None{}, nil
 	}
 
 	clusters := make([]output.TableRow, 0)
@@ -75,16 +102,8 @@ func (s *configCommand) Execute(exec commands.Executor, uuid string) (output.Out
 		})
 	}
 
-	exec.PushProgressSuccess(msg)
-
-	if s.Cobra().Flag("write").Changed {
-		msg := fmt.Sprintf("Writing kubeconfig for Kubernetes cluster %s to destination %s", uuid, s.write)
-		exec.PushProgressStarted(msg)
-		exec.PushProgressSuccess(msg)
-	}
-
 	return output.MarshaledWithHumanOutput{
-		Value: kubeconfig,
+		Value: resp,
 		Output: output.Combined{
 			output.CombinedSection{
 				Contents: output.Details{
@@ -122,4 +141,20 @@ func (s *configCommand) Execute(exec commands.Executor, uuid string) (output.Out
 			},
 		},
 	}, nil
+}
+
+func mergeConfig(startingConfig, newConfig *api.Config) api.Config {
+	startingConfig.CurrentContext = newConfig.CurrentContext
+
+	for k, v := range newConfig.Clusters {
+		startingConfig.Clusters[k] = v
+	}
+	for k, v := range newConfig.AuthInfos {
+		startingConfig.AuthInfos[k] = v
+	}
+	for k, v := range newConfig.Contexts {
+		startingConfig.Contexts[k] = v
+	}
+
+	return *startingConfig
 }
