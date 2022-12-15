1198f8b54973eb67a81e196d9e78989318cc744d
villevsv-upcloud <110451292+villevsv-upcloud@users.noreply.github.com>
2022-12-15 17:53:10 +0200

    feat(kubernetes): config (#195)
    Add `kubernetes config` command to be printed via stdout or written in a
    given file.

--- go.mod
+++ go.mod
@@ -10,14 +10,15 @@ require (
 	github.com/gemalto/flume v0.12.0
 	github.com/jedib0t/go-pretty/v6 v6.3.3
 	github.com/m7shapan/cidr v0.0.0-20200427124835-7eba0889a5d2
-	github.com/mattn/go-isatty v0.0.12
+	github.com/mattn/go-isatty v0.0.16
 	github.com/spf13/cobra v1.5.0
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.8.0
-	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5
-	golang.org/x/term v0.0.0-20220526004731-065cf7ba2467
+	golang.org/x/crypto v0.0.0-20220315160706-3147a52a75dd
+	golang.org/x/term v0.2.0
 	gopkg.in/yaml.v3 v3.0.1
+	k8s.io/client-go v0.25.4
 )
 
 require (
@@ -25,21 +26,26 @@ require (
 	github.com/blang/semver v3.5.1+incompatible // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/gofuzz v1.1.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/imdario/mergo v0.3.6 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/kr/text v0.2.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/magiconair/properties v1.8.1 // indirect
-	github.com/mattn/go-colorable v0.1.6 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
 	github.com/mitchellh/mapstructure v1.3.3 // indirect
-	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml v1.2.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/spf13/afero v1.1.2 // indirect
+	github.com/spf13/afero v1.2.2 // indirect
 	github.com/spf13/cast v1.3.0 // indirect
 	github.com/spf13/jwalterweatherman v1.0.0 // indirect
 	github.com/stretchr/objx v0.4.0 // indirect
@@ -47,12 +53,23 @@ require (
 	go.uber.org/atomic v1.6.0 // indirect
 	go.uber.org/multierr v1.5.0 // indirect
 	go.uber.org/zap v1.13.0 // indirect
-	golang.org/x/lint v0.0.0-20190930215403-16217165b5de // indirect
-	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
-	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/tools v0.0.0-20200103221440-774c71fcf114 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
+	golang.org/x/net v0.2.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+	golang.org/x/time v0.0.0-20220210224613-90d013bbcef8 // indirect
+	golang.org/x/tools v0.3.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/ini.v1 v1.51.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	k8s.io/apimachinery v0.25.4 // indirect
+	k8s.io/klog/v2 v2.70.1 // indirect
+	k8s.io/utils v0.0.0-20220728103510-ee6ede2d64ed // indirect
+	sigs.k8s.io/json v0.0.0-20220713155537-f223a00ba0e2 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
+	sigs.k8s.io/yaml v1.2.0 // indirect
 )
---
+++ internal/commands/kubernetes/config.go
@@ -0,0 +1,180 @@
+package kubernetes
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/UpCloudLtd/upcloud-cli/v2/internal/commands"
+	"github.com/UpCloudLtd/upcloud-cli/v2/internal/completion"
+	"github.com/UpCloudLtd/upcloud-cli/v2/internal/output"
+	"github.com/UpCloudLtd/upcloud-cli/v2/internal/resolver"
+
+	"github.com/UpCloudLtd/upcloud-go-api/v4/upcloud/request"
+	"github.com/spf13/pflag"
+	"gopkg.in/yaml.v3"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/clientcmd/api"
+)
+
+type configCommand struct {
+	*commands.BaseCommand
+	resolver.CachingKubernetes
+	completion.Kubernetes
+	pathOptions *clientcmd.PathOptions
+}
+
+// ConfigCommand creates the "connection config" command
+func ConfigCommand() commands.Command {
+	return &configCommand{
+		BaseCommand: commands.New(
+			"config",
+			"Output Kubernetes cluster kubeconfig",
+			`upctl kubernetes config 0fa980c4-0e4f-460b-9869-11b7bd62b831 --output human`,
+			`upctl kubernetes config 0fa980c4-0e4f-460b-9869-11b7bd62b831 --output yaml --write $KUBECONFIG`,
+			`upctl kubernetes config 0fa980c4-0e4f-460b-9869-11b7bd62b831 --output yaml --write ./my_kubeconfig.yaml`,
+		),
+		pathOptions: clientcmd.NewDefaultPathOptions(),
+	}
+}
+
+// InitCommand implements Command.InitCommand
+func (c *configCommand) InitCommand() {
+	flagSet := &pflag.FlagSet{}
+	flagSet.StringVar(
+		&c.pathOptions.LoadingRules.ExplicitPath,
+		"write",
+		"",
+		"Absolute path for writing output. If the file exists, the config will be merged.")
+	c.AddFlags(flagSet)
+}
+
+// Execute implements commands.MultipleArgumentCommand
+func (c *configCommand) Execute(exec commands.Executor, uuid string) (output.Output, error) {
+	svc := exec.All()
+
+	msg := fmt.Sprintf("Getting kubeconfig for Kubernetes cluster %s", uuid)
+	exec.PushProgressStarted(msg)
+
+	resp, err := svc.GetKubernetesKubeconfig(&request.GetKubernetesKubeconfigRequest{
+		UUID: uuid,
+	})
+	if err != nil {
+		return commands.HandleError(exec, msg, err)
+	}
+
+	config, err := clientcmd.Load([]byte(resp))
+	if err != nil {
+		return commands.HandleError(exec, msg, err)
+	}
+
+	exec.PushProgressSuccess(msg)
+
+	if c.Cobra().Flag("write").Changed {
+		return c.write(exec, uuid, config)
+	}
+
+	return c.output(exec, config, resp, msg)
+}
+
+func (c *configCommand) output(exec commands.Executor, config *api.Config, resp string, msg string) (output.Output, error) {
+	clusters := make([]output.TableRow, 0)
+	for _, v := range config.Clusters {
+		clusters = append(clusters, output.TableRow{
+			v.Server,
+			v.InsecureSkipTLSVerify,
+		})
+	}
+
+	contexts := make([]output.TableRow, 0)
+	for _, v := range config.Contexts {
+		contexts = append(contexts, output.TableRow{
+			v.Cluster,
+			v.AuthInfo,
+		})
+	}
+
+	var value interface{}
+	err := yaml.Unmarshal([]byte(resp), &value)
+	if err != nil {
+		return commands.HandleError(exec, msg, err)
+	}
+
+	return output.MarshaledWithHumanOutput{
+		Value: value,
+		Output: output.Combined{
+			output.CombinedSection{
+				Contents: output.Details{
+					Sections: []output.DetailSection{
+						{
+							Title: "Overview:",
+							Rows: []output.DetailRow{
+								{Title: "Current context:", Value: config.CurrentContext},
+							},
+						},
+					},
+				},
+			},
+			output.CombinedSection{
+				Title: "Clusters",
+				Contents: output.Table{
+					Columns: []output.TableColumn{
+						{Key: "server", Header: "Server"},
+						{Key: "insecure_skip_tls_verify", Header: "Insecure skip TLS verify"},
+					},
+					Rows:       clusters,
+					HideHeader: false,
+				},
+			},
+			output.CombinedSection{
+				Title: "Contexts",
+				Contents: output.Table{
+					Columns: []output.TableColumn{
+						{Key: "cluster", Header: "Cluster"},
+						{Key: "authinfo", Header: "Authinfo"},
+					},
+					Rows:       contexts,
+					HideHeader: false,
+				},
+			},
+		},
+	}, nil
+}
+
+func (c *configCommand) write(exec commands.Executor, uuid string, config *api.Config) (output.Output, error) {
+	msg := fmt.Sprintf("Writing kubeconfig for Kubernetes cluster %s to destination %s", uuid, c.pathOptions.GetDefaultFilename())
+	exec.PushProgressStarted(msg)
+
+	if c.Cobra().Flag("write").Value.String() == c.Cobra().Flag("write").DefValue {
+		return commands.HandleError(exec, msg, errors.New("invalid write path"))
+	}
+
+	startingConfig, err := c.pathOptions.GetStartingConfig()
+	if err != nil {
+		return commands.HandleError(exec, msg, err)
+	}
+
+	err = clientcmd.ModifyConfig(c.pathOptions, mergeConfig(startingConfig, config), false)
+	if err != nil {
+		return commands.HandleError(exec, msg, err)
+	}
+
+	exec.PushProgressSuccess(msg)
+
+	return output.None{}, nil
+}
+
+func mergeConfig(startingConfig, newConfig *api.Config) api.Config {
+	startingConfig.CurrentContext = newConfig.CurrentContext
+
+	for k, v := range newConfig.Clusters {
+		startingConfig.Clusters[k] = v
+	}
+	for k, v := range newConfig.AuthInfos {
+		startingConfig.AuthInfos[k] = v
+	}
+	for k, v := range newConfig.Contexts {
+		startingConfig.Contexts[k] = v
+	}
+
+	return *startingConfig
+}
