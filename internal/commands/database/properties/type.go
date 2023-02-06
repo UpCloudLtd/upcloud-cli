0a61f537852678ff96b5624847054a3566721efe
Toni Kangas <toni.kangas@upcloud.com>
2023-02-06 17:47:22 +0200

    fix(database): use plurar in `properties list` description

--- internal/commands/database/properties/type.go
+++ internal/commands/database/properties/type.go
@@ -13,7 +13,7 @@ import (
 // creates the "database properties <serviceType>" command
 func DbTypeCommand(serviceType string, serviceName string) commands.Command {
 	return &dbTypeCommand{
-		BaseCommand: commands.New(serviceType, fmt.Sprintf("List available properties for %s database", serviceName), fmt.Sprintf("upctl database properties %s", serviceType)),
+		BaseCommand: commands.New(serviceType, fmt.Sprintf("List available properties for %s databases", serviceName), fmt.Sprintf("upctl database properties %s", serviceType)),
 		serviceType: serviceType,
 	}
 }
