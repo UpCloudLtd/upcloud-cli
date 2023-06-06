d33242e7e62b718ea9f1bd5f84f5ce1135d3f0b1
Toni Kangas <toni.kangas@upcloud.com>
2023-06-06 15:01:01 +0300

    fix: update docstring

--- internal/namedargs/text.go
+++ internal/namedargs/text.go
@@ -2,7 +2,7 @@ package namedargs
 
 import "fmt"
 
-// Returns description for --zone argument, e.g. "Zone where to create the resource".
+// Returns description for --zone argument, e.g. "Zone where to create the resource...".
 func ZoneDescription(resource string) string {
 	return fmt.Sprintf("Zone where to create the %s. Run `upctl zone list` to list all available zones.", resource)
 }
