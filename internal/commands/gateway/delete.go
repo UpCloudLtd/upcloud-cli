7a76c3c042c6a8703086123b6d060c28ac18afb3
Soner Cem Kebiz <soner.kebiz@upcloud.com>
2024-02-07 17:14:43 +0200

    feat(gateway): add commands for list & delete (#289)

---
+++ internal/commands/gateway/delete.go
@@ -0,0 +1,48 @@
+package gateway
+
+import (
+	"fmt"
+
+	"github.com/UpCloudLtd/upcloud-cli/v3/internal/commands"
+	"github.com/UpCloudLtd/upcloud-cli/v3/internal/completion"
+	"github.com/UpCloudLtd/upcloud-cli/v3/internal/output"
+	"github.com/UpCloudLtd/upcloud-cli/v3/internal/resolver"
+
+	"github.com/UpCloudLtd/upcloud-go-api/v6/upcloud/request"
+)
+
+// DeleteCommand creates the "gateway delete" command
+func DeleteCommand() commands.Command {
+	return &deleteCommand{
+		BaseCommand: commands.New(
+			"delete",
+			"Delete a gateway",
+			"upctl gateway delete 8abc8009-4325-4b23-4321-b1232cd81231",
+			"upctl gateway delete my-gateway",
+		),
+	}
+}
+
+type deleteCommand struct {
+	*commands.BaseCommand
+	resolver.CachingGateway
+	completion.Gateway
+}
+
+// Execute implements commands.MultipleArgumentCommand
+func (c *deleteCommand) Execute(exec commands.Executor, arg string) (output.Output, error) {
+	svc := exec.All()
+	msg := fmt.Sprintf("Deleting gateway %v", arg)
+	exec.PushProgressStarted(msg)
+
+	err := svc.DeleteGateway(exec.Context(), &request.DeleteGatewayRequest{
+		UUID: arg,
+	})
+	if err != nil {
+		return commands.HandleError(exec, msg, err)
+	}
+
+	exec.PushProgressSuccess(msg)
+
+	return output.None{}, nil
+}
---
+++ internal/resolver/gateway_test.go
@@ -0,0 +1,91 @@
+package resolver_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	smock "github.com/UpCloudLtd/upcloud-cli/v3/internal/mock"
+	"github.com/UpCloudLtd/upcloud-cli/v3/internal/resolver"
+
+	"github.com/UpCloudLtd/upcloud-go-api/v6/upcloud"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+var mockGateways = []upcloud.Gateway{
+	{Name: "asd", UUID: "abcdef"},
+	{Name: "asd", UUID: "abcghi"},
+	{Name: "qwe", UUID: "jklmno"},
+}
+
+func TestGatewayResolution(t *testing.T) {
+	t.Run("UUID", func(t *testing.T) {
+		mService := &smock.Service{}
+		mService.On("GetGateways", mock.Anything).Return(mockGateways, nil)
+		res := resolver.CachingGateway{}
+		argResolver, err := res.Get(context.TODO(), mService)
+		assert.NoError(t, err)
+		for _, db := range mockGateways {
+			resolved, err := argResolver(db.UUID)
+			assert.NoError(t, err)
+			assert.Equal(t, db.UUID, resolved)
+		}
+
+		// Make sure caching works, eg. we didn't call GetGateways more than once
+		mService.AssertNumberOfCalls(t, "GetGateways", 1)
+	})
+
+	t.Run("Name", func(t *testing.T) {
+		mService := &smock.Service{}
+		mService.On("GetGateways", mock.Anything).Return(mockGateways, nil)
+		res := resolver.CachingGateway{}
+		argResolver, err := res.Get(context.TODO(), mService)
+		assert.NoError(t, err)
+
+		db := mockGateways[2]
+		resolved, err := argResolver(db.Name)
+		assert.NoError(t, err)
+		assert.Equal(t, db.UUID, resolved)
+		// Make sure caching works, eg. we didn't call GetGateways more than once
+		mService.AssertNumberOfCalls(t, "GetGateways", 1)
+	})
+
+	t.Run("Failures", func(t *testing.T) {
+		mService := &smock.Service{}
+		mService.On("GetGateways", mock.Anything).Return(mockGateways, nil)
+
+		res := resolver.CachingGateway{}
+		argResolver, err := res.Get(context.TODO(), mService)
+		assert.NoError(t, err)
+		var resolved string
+
+		// Ambigous Name
+		resolved, err = argResolver("asd")
+		if !assert.Error(t, err) {
+			t.FailNow()
+		}
+		assert.ErrorIs(t, err, resolver.AmbiguousResolutionError("asd"))
+		assert.Equal(t, "", resolved)
+
+		// Not found
+		resolved, err = argResolver("not-found")
+		if !assert.Error(t, err) {
+			t.FailNow()
+		}
+		assert.ErrorIs(t, err, resolver.NotFoundError("not-found"))
+		assert.Equal(t, "", resolved)
+
+		// Make sure caching works, eg. we didn't call GetGateways more than once
+		mService.AssertNumberOfCalls(t, "GetGateways", 1)
+	})
+}
+
+func TestFailingGatewayResolution(t *testing.T) {
+	mService := &smock.Service{}
+	mService.On("GetGateways", mock.Anything).Return(nil, errors.New("MOCKERROR"))
+	res := resolver.CachingGateway{}
+	argResolver, err := res.Get(context.TODO(), mService)
+	assert.EqualError(t, err, "MOCKERROR")
+	assert.Nil(t, argResolver)
+}
