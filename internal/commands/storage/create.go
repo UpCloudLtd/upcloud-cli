69b1542235e3cabd82b5816f0653c6ce2992f3dd
Zim Kalinowski <zim.kalinowski@zoho.com>
2024-10-18 10:39:03 +0200

    fix: small inconsistencies in help texts (#336)
    Co-authored-by: Zim Kalinowski <zim.kalinowski@espressif.com>

--- internal/commands/storage/create.go
+++ internal/commands/storage/create.go
@@ -83,7 +83,7 @@ func applyCreateFlags(fs *pflag.FlagSet, dst, def *createParams) {
 	fs.StringVar(&dst.Tier, "tier", def.Tier, "Storage tier.")
 	config.AddToggleFlag(fs, &dst.encrypted, "encrypt", false, "Encrypt the storage.")
 	fs.StringVar(&dst.backupTime, "backup-time", def.backupTime, "The time when to create a backup in HH:MM. Empty value means no backups.")
-	fs.StringVar(&dst.BackupRule.Interval, "backup-interval", def.BackupRule.Interval, "The interval of the backup.\nAvailable: daily,mon,tue,wed,thu,fri,sat,sun")
+	fs.StringVar(&dst.BackupRule.Interval, "backup-interval", def.BackupRule.Interval, "The interval of the backup.\nAvailable: daily, mon, tue, wed, thu, fri, sat, sun")
 	fs.IntVar(&dst.BackupRule.Retention, "backup-retention", def.BackupRule.Retention, "How long to store the backups in days. The accepted range is 1-1095")
 }
 
