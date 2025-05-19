3f95752ffdd11d657cfba76f67d6dd9d2b66010a
Toni Kangas <toni.kangas@upcloud.com>
2025-05-19 19:21:10 +0300

    feat(all): add support for load balancers

--- internal/resolver/loadbalancer.go
+++ internal/resolver/loadbalancer.go
@@ -4,21 +4,32 @@ import (
 	"context"
 
 	internal "github.com/UpCloudLtd/upcloud-cli/v3/internal/service"
+	"github.com/UpCloudLtd/upcloud-go-api/v8/upcloud"
 	"github.com/UpCloudLtd/upcloud-go-api/v8/upcloud/request"
 )
 
 // CachingLoadBalancer implements resolver for servers, caching the results
-type CachingLoadBalancer struct{}
+type CachingLoadBalancer struct {
+	Cache[upcloud.LoadBalancer]
+}
 
 // make sure we implement the ResolutionProvider interface
-var _ ResolutionProvider = CachingLoadBalancer{}
+var (
+	_ ResolutionProvider                              = &CachingLoadBalancer{}
+	_ CachingResolutionProvider[upcloud.LoadBalancer] = &CachingLoadBalancer{}
+)
 
 // Get implements ResolutionProvider.Get
-func (s CachingLoadBalancer) Get(ctx context.Context, svc internal.AllServices) (Resolver, error) {
+func (s *CachingLoadBalancer) Get(ctx context.Context, svc internal.AllServices) (Resolver, error) {
 	loadbalancers, err := svc.GetLoadBalancers(ctx, &request.GetLoadBalancersRequest{})
 	if err != nil {
 		return nil, err
 	}
+
+	for _, loadbalancer := range loadbalancers {
+		s.AddCached(loadbalancer.UUID, loadbalancer)
+	}
+
 	return func(arg string) Resolved {
 		rv := Resolved{Arg: arg}
 		for _, lb := range loadbalancers {
