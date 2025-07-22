2a713d47f2a83ddccf6329c2f6452823fa7e3ef0
Toni Kangas <toni.kangas@upcloud.com>
2025-07-22 17:28:14 +0300

    refactor(completion): remove custom whitespace handling
    The latest cobra version handles bash completions also when value contains whitespace, so replacing whitespace with non-breaking spaces is not needed anymore.

--- internal/completion/helpers.go
+++ internal/completion/helpers.go
@@ -1,18 +1,8 @@
 package completion
 
 import (
-	"regexp"
 	"strings"
 )
-
-var oneOrMoreSpace = regexp.MustCompile(` +`)
-
-// RemoveWordBreaks replaces all whitespaces in input strings with non-breaking spaces to prevent bash from splitting completion with whitespace into multiple completions.
-//
-// This hack allows us to use cobras built-in completion logic and can be removed once cobra supports whitespace in bash completions (See https://github.com/spf13/cobra/issues/1740).
-func RemoveWordBreaks(input string) string {
-	return oneOrMoreSpace.ReplaceAllString(input, "\u00A0")
-}
 
 // MatchStringPrefix returns a list of string in vals which have a prefix as specified in key. Quotes are removed from key and output strings are escaped according to completion rules
 func MatchStringPrefix(vals []string, key string, caseSensitive bool) []string {
@@ -22,7 +12,7 @@ func MatchStringPrefix(vals []string, key string, caseSensitive bool) []string {
 		if (caseSensitive && strings.HasPrefix(v, key)) ||
 			(!caseSensitive && strings.HasPrefix(strings.ToLower(v), strings.ToLower(key))) ||
 			key == "" {
-			r = append(r, RemoveWordBreaks(v))
+			r = append(r, v)
 		}
 	}
 	return r
--- internal/resolver/matchers.go
+++ internal/resolver/matchers.go
@@ -3,22 +3,20 @@ package resolver
 import (
 	"path/filepath"
 	"strings"
-
-	"github.com/UpCloudLtd/upcloud-cli/v3/internal/completion"
 )
 
-// MatchStringWithWhitespace checks if arg that may include whitespace matches given value. This checks both quoted args and auto-completed args handled with completion.RemoveWordBreaks.
-func MatchArgWithWhitespace(arg, value string) MatchType {
-	if completion.RemoveWordBreaks(value) == arg || value == arg {
+// MatchArgWithEqualFold checks if arg is a exact or case-insensitive match.
+func MatchArgWithEqualFold(arg, value string) MatchType {
+	if value == arg {
 		return MatchTypeExact
 	}
-	if strings.EqualFold(completion.RemoveWordBreaks(value), arg) || strings.EqualFold(value, arg) {
+	if strings.EqualFold(value, arg) {
 		return MatchTypeCaseInsensitive
 	}
 	return MatchTypeNone
 }
 
-// MatchStringWithWhitespace checks if arg matches given value as a unix style glob pattern.
+// MatchArgWithGlobPattern checks if arg matches given value as a unix style glob pattern.
 func MatchArgWithGlobPattern(arg, value string) MatchType {
 	if matched, _ := filepath.Match(arg, value); matched {
 		return MatchTypeGlobPattern
@@ -26,11 +24,11 @@ func MatchArgWithGlobPattern(arg, value string) MatchType {
 	return MatchTypeNone
 }
 
-// MatchTitle checks if arg matches any of the given values by using MatchArgWithWhitespace and MatchArgWithGlobPattern matchers.
+// MatchTitle checks if arg matches any of the given values by using MatchArgWithEqualFold and MatchArgWithGlobPattern matchers.
 func MatchTitle(arg string, values ...string) MatchType {
 	match := MatchTypeNone
 	for _, value := range values {
-		match = max(match, MatchArgWithWhitespace(arg, value))
+		match = max(match, MatchArgWithEqualFold(arg, value))
 		match = max(match, MatchArgWithGlobPattern(arg, value))
 	}
 	return match
